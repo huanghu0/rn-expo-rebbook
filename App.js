8426e961c755e254a4de6396b23cfe3f7e202ccb
huanghu0 <1525273035@qq.com>
2024-01-18 23:55:56 +0800

    feat:expo 项目初始化

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>你好我是你爸爸</Text>
       <StatusBar style="auto" />
     </View>
   );
