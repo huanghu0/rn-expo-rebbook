1170000c549de085a67b13da0418f4ec2e870327
huanghu0 <1525273035@qq.com>
2024-01-21 00:18:48 +0800

    feat:使用图像选择

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import ImageViewer from './Components/ImageViewer'
+import Button from './Components/Button'
+import PlaceholderImage from './assets/images/background-image.png'
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react'
 
 export default function App() {
+  const [ selectedImage,setSelectedImage ] = useState(null)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('You did not select any image.');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>你好我是你爸爸</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}></ImageViewer>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="choose a photo" theme="primary" onPress={pickImageAsync}  />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer:{
+    flex:1,
+    paddingTop:58
+  },
+  footerContainer:{
+    flex: 1 / 3,
+    alignItems:'center'
+  }
 });
